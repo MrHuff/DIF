9edca033b6d888e31ef29744257029c94ef40e0e
rhu <robert.hu@stats.ox.ac.uk>
2020-08-29 23:22:19 +0200

    fix

--- utils/loglikelihood.py
+++ utils/loglikelihood.py
@@ -20,30 +20,30 @@ def estimate_loglikelihoods(dataloader_test, model,s=1000):
     _loglikelihood_estimates = []
     _elbo_estimates = []
     _class = []
-    for iteration, (batch, c) in enumerate(tqdm.tqdm(dataloader_test)):
-        _elbo = []
-        for i in tqdm.trange(s):
-            with autocast():
-                ELBO = calculate_ELBO(model,batch.cuda())
-            _elbo.append(ELBO)
-        _elbo_estimates.append(ELBO)
-        likelihood_est = torch.stack(_elbo,dim=1)
-        _loglikelihood_estimates.append(likelihood_est.mean(-1))
-        _class.append(c)
-    _elbo_estimates = torch.cat(_elbo_estimates,dim=0)
-    _class = torch.cat(_class,dim=0)
-    _loglikelihood_estimates = torch.cat(_loglikelihood_estimates,dim=0)
-    return _loglikelihood_estimates,_elbo_estimates,_class
-
-def loglikelihood_est(_loglikelihood_estimates):
-    return torch.logsumexp(_loglikelihood_estimates, 0).cpu() - torch.log(torch.tensor(_loglikelihood_estimates.shape[0]).float())
+    tensor_s = torch.tensor(s).float()
+    with torch.no_grad():
+        for iteration, (batch, c) in enumerate(tqdm.tqdm(dataloader_test)):
+            _elbo = []
+            for i in tqdm.trange(s):
+                with autocast():
+                    ELBO = calculate_ELBO(model,batch.cuda())
+                _elbo.append(ELBO)
+            _elbo_estimates.append(ELBO)
+            likelihood_est = torch.stack(_elbo,dim=1)
+            # print(torch.logsumexp(likelihood_est,dim=1).cpu()-torch.log(tensor_s))
+            _loglikelihood_estimates.append(torch.logsumexp(likelihood_est,dim=1).cpu()-torch.log(tensor_s))
+            _class.append(c)
+        _elbo_estimates = torch.cat(_elbo_estimates,dim=0)
+        _class = torch.cat(_class,dim=0)
+        _loglikelihood_estimates = torch.cat(_loglikelihood_estimates,dim=0)
+        return _loglikelihood_estimates,_elbo_estimates,_class
 
 def calculate_metrics(_loglikelihood_estimates,_elbo_estimates,_class):
     with torch.no_grad():
-        loglikelihood_estimate = loglikelihood_est(_loglikelihood_estimates)
+        loglikelihood_estimate = _loglikelihood_estimates.mean(0)
         ELBO = _elbo_estimates.mean()
-        loglikelihood_estimate_A = loglikelihood_est(_loglikelihood_estimates[~_class])
-        loglikelihood_estimate_B = loglikelihood_est(_loglikelihood_estimates[_class])
+        loglikelihood_estimate_A =_loglikelihood_estimates[~_class].mean(0)
+        loglikelihood_estimate_B = _loglikelihood_estimates[_class].mean(0)
         ELBO_A = _elbo_estimates[~_class].mean()
         ELBO_B = _elbo_estimates[_class].mean()
 
